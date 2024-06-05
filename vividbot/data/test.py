14ca3fab347bb1db028a2d9ab8525e3333a6d0a7
duytran <crackonebrain@gmail.com>
2024-06-05 21:33:34 +0700

    chore: update vast2m

--- vividbot/data/test.py
+++ vividbot/data/test.py
@@ -1,18 +1,24 @@
 import os
 import sys
+from datasets import load_dataset
 
 sys.path.append(os.getcwd())
 
 from vividbot.data.processor.upload_hf import Uploader
 
 uploader = Uploader()
-path = "/home/duytran/Downloads/output/instruct500k_vi.json"
+path = "/home/duytran/Downloads/output_ds/vast_2m_final.json"
+
+# uploader.upload_dir(dir_path=path, 
+#                     repo_id="Vividbot/vast2m_vi", 
+#                     path_in_repo="metadata", 
+#                     repo_type="dataset", overwrite=True)
 
 uploader.upload_file(file_path=path,
-                     repo_id= "Vividbot/instruct500k_vi",
-                     path_in_repo= "instruct500k_vi.json",
+                     repo_id= "Vividbot/vast2m_vi",
+                     path_in_repo= "vast2m_vi.json",
                      repo_type= "dataset",
-                     overwrite= True,)
+                     overwrite= True)
 
 # for folder in os.listdir(path):
 #     uploader.zip_and_upload_dir(
@@ -22,3 +28,43 @@ uploader.upload_file(file_path=path,
 #         repo_type="dataset",
 #         overwrite=False,
 #     )
+# from huggingface_hub import HfFileSystem
+# import os
+# import zipfile
+# from datasets import load_dataset
+
+# from tqdm import tqdm
+
+# fs = HfFileSystem()
+# input = "/home/duytran/Downloads/output_ds/vast_2m_chunk_en2vi"
+# output = "/home/duytran/Downloads/output_ds/vast_2m_final"
+
+# def rename_path(batch):
+#         batch['video'] = [file_name + "/" + item for item in batch['video']]
+#         return batch
+# error = []
+# for file_name in tqdm(os.listdir(input)):
+#     path = os.path.join(input, file_name)
+    
+#     file_name = os.path.basename(path).split(".")[0]
+#     print("-"*50, file_name, "-"*50)
+#     dataset = load_dataset("json", data_files=path)["train"]
+#     dataset = dataset.map(rename_path, 
+#                         batched=True,
+#                         num_proc=8)
+    
+#     path_hf = "datasets/Vividbot/vast2m_vi/video/" + file_name + ".zip"
+#     zip_hf = fs.open(path_hf)
+#     with zipfile.ZipFile(zip_hf, 'r') as zip_ref:
+#         list_files = zip_ref.namelist()[1:]
+#         len_zip = len(list_files)
+#         new_data = dataset.filter(lambda x: x["video"] in list_files, num_proc=8)
+#         print("length of new data: ", len(new_data))
+#         print("length of zip: ", len_zip)
+#         assert len(new_data) <= len_zip, "Not enough video in zip"
+#         new_data.to_json(output + "/" + file_name + ".json", force_ascii=False)
+#         print(f"Save to {output}/{file_name}.json")
+#     zip_hf.close()
+
+
+    
--- vividbot/script/vast2m.sh
+++ vividbot/script/vast2m.sh
@@ -1,22 +1,22 @@
-python vividbot/data/task/vast2M.py --task "generate" \
-                                    --file-path "/home/duytran/Downloads/output_ds/vast_2m_chunk_en" \
-                                    --select -1 \
-                                    --batch-size 100 \
-                                    --repo-id "Vividbot/vast2m_vi" \
-                                    --num-shards 10 \
-                                    --output-dir "/home/duytran/Downloads/output_ds/vast_2m_chunk_en2vi" \
-                                    --cache-dir "/home/duytran/Downloads/cache_dir" \
-                                    --num-proc 16 
-
-# python3 vividbot/data/task/vast2M.py --task "download" \
-#                                     --file-path "/home/duytran/Downloads/output_ds/chunk_200" \
+# python vividbot/data/task/vast2M.py --task "generate" \
+#                                     --file-path "/home/duytran/Downloads/output_ds/vast_2m_chunk_en" \
+#                                     --select -1 \
 #                                     --batch-size 100 \
 #                                     --repo-id "Vividbot/vast2m_vi" \
-#                                     --upload-to-hub \
 #                                     --num-shards 10 \
-#                                     --output-dir "/home/duytran/Downloads/output_video" \
+#                                     --output-dir "/home/duytran/Downloads/output_ds/vast_2m_chunk_en2vi" \
 #                                     --cache-dir "/home/duytran/Downloads/cache_dir" \
-#                                     --num-proc 16
+#                                     --num-proc 16 
+
+python3 vividbot/data/task/vast2M.py --task "download" \
+                                    --file-path "/home/duytran/Downloads/output_ds/vast_2m_chunk_en" \
+                                    --batch-size 100 \
+                                    --repo-id "Vividbot/vast2m_vi" \
+                                    --upload-to-hub \
+                                    --num-shards 10 \
+                                    --output-dir "/home/duytran/Downloads/output_video" \
+                                    --cache-dir "/home/duytran/Downloads/cache_dir" \
+                                    --num-proc 16
 
 # python vividbot/data/task/vast2M.py --task "rename column" \
 #                                     --file-path "/home/duytran/Downloads/output_ds/vast2M_vi.json" \
