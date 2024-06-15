1ba92cb47b7b7fd8429ed8fd2c5e8038c67df440
Arphew <thach.nguyen@silicondynamics.io>
2024-06-15 22:48:23 +0700

    update import

--- Valley/valley/train/train.py
+++ Valley/valley/train/train.py
@@ -7,7 +7,7 @@ from peft import get_peft_model, LoraConfig, TaskType
 import torch
 import transformers
 from transformers import TrainerCallback
-from sequentialTrainner import Trainer
+from valley.train.sequentialTrainner import Trainer
 from valley.train.trainner import LLMCallback
 from valley.model.valley_model import ValleyLlamaForCausalLM
 from valley.util.data_util import smart_tokenizer_and_embedding_resize, safe_save_model_for_hf_trainer
@@ -100,7 +100,6 @@ def train(args):
     parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_yaml_file(args.conf,allow_extra_keys=True)
     training_args.learning_rate = float(training_args.learning_rate)
-    
     os.environ['WANDB_PROJECT'] = data_args.project_name
 
     model = ValleyLlamaForCausalLM.from_pretrained(
