72f1f306cda3a78c55b9808b4a904157fbcaaf64
Minh Vu <minhngoc02hn@gmail.com>
2024-07-25 10:01:01 +0700

    safe langfuse init

--- vividbot/data/task/vivid_instruct_65k/video.py
+++ vividbot/data/task/vivid_instruct_65k/video.py
@@ -8,6 +8,7 @@ import google.generativeai as genai
 import numpy as np
 from datasets import load_dataset
 from dotenv import load_dotenv
+from langfuse.callback import CallbackHandler
 from tqdm import tqdm
 
 from vividbot.data.processor.download import YoutubeDownloader
@@ -164,7 +165,14 @@ def _process(batch: dict):
           },
         )
         try:
-          langfuse_handler.session_id = video_id_with_chunk_id
+          langfuse_handler = CallbackHandler(
+            secret_key="sk-lf-a96d608f-1edd-4b30-bf76-3db501c6e9ff",
+            public_key="pk-lf-94bc2370-6aec-4671-b8fe-3ddcc099d5d1",
+            host="https://langfuse.formularizer.com",
+            tags=["vividbot"],
+            session_id=video_id_with_chunk_id,
+          )
+
           response: str = GENERATE_QA_PAIRS_CHAIN.invoke(
             {"message": describer_response.text.strip()},
             {"callbacks": [langfuse_handler]},
