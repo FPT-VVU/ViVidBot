85230fd2a68bf23d9a9d47439fecd00d4a580b3a
Minh Vu <minhngoc02hn@gmail.com>
2024-07-25 19:24:31 +0700

    change langfuse project

--- vividbot/data/task/vivid_instruct_65k/video.py
+++ vividbot/data/task/vivid_instruct_65k/video.py
@@ -166,8 +166,8 @@ def _process(batch: dict):
         )
         try:
           langfuse_handler = CallbackHandler(
-            secret_key="sk-lf-a96d608f-1edd-4b30-bf76-3db501c6e9ff",
-            public_key="pk-lf-94bc2370-6aec-4671-b8fe-3ddcc099d5d1",
+            secret_key="sk-lf-bfa7365e-2871-4669-bda2-4818fcab68de",
+            public_key="pk-lf-b6e63d27-8f4b-4911-acd2-5838400c1dfb",
             host="https://langfuse.formularizer.com",
             tags=["vividbot"],
             session_id=video_id_with_chunk_id,
@@ -427,7 +427,7 @@ def main():
     key=lambda x: int(x.split(".")[0].split("_")[1]),
   )
 
-  last_successful_shard = 27
+  last_successful_shard = 43
   # only process shards after the last successful shard
   shard_files = shard_files[last_successful_shard + 1 :]
 
