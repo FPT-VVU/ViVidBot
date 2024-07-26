593e983cf3ce86af43203d0b330c8529103d6c79
Minh Vu <minhvu@codertrove.com>
2024-07-26 11:28:40 +0700

    fix

--- vividbot/data/task/vivid_instruct_65k/video.py
+++ vividbot/data/task/vivid_instruct_65k/video.py
@@ -468,7 +468,7 @@ def main():
     key=lambda x: int(x.split(".")[0].split("_")[1]),
   )
 
-  last_successful_shard = 76
+  last_successful_shard = 77
   # only process shards after the last successful shard
   shard_files = shard_files[last_successful_shard + 1 :]
 
