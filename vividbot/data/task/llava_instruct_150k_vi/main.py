134e4567d3edb5a53a5137abff7675a2bb618b50
Minh Vu <minhvu@codertrove.com>
2024-07-30 14:44:08 +0700

    add llava-instruct-150k-vi processor

---
+++ vividbot/data/task/llava_instruct_150k_vi/main.py
@@ -0,0 +1,117 @@
+import logging
+import os
+import time
+from pathlib import Path
+
+import requests
+from datasets import load_dataset
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+from vividbot.data.processor.huggingface import HuggingFaceProcessor
+from vividbot.data.task.vivid_instruct_65k.utils.notifications import (
+  send_completion_message,
+)
+
+load_dotenv()
+BASE_DATA_PATH = f"{Path.home()}/llava-instruct-150k-vi"
+COCO_BASE_URL = "http://images.cocodataset.org/train2014/COCO_train2014_"
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+  filename=f"{BASE_DATA_PATH}/run.log",
+  filemode="w",
+  level=logging.INFO,
+  format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+hf_processor = HuggingFaceProcessor()
+
+
+def _process(batch: dict):
+  for id, image in zip(batch["id"], batch["image"]):
+    # image: str = "shard_0/000000000000.jpg"
+    # id: str = "000000000000"
+
+    if os.path.exists(f"{BASE_DATA_PATH}/images/{image}"):
+      continue
+
+    image_url = f"{COCO_BASE_URL}{id}.jpg"
+
+    response = requests.get(image_url)
+    with open(f"{BASE_DATA_PATH}/images/{image}", "wb") as f:
+      f.write(response.content)
+
+
+def process(shard_filename: str):
+  """
+  shard_filename: str = "shard_0.json"
+  """
+  start_time = time.time()
+
+  shard = shard_filename.split(".")[0]
+
+  os.makedirs(f"{BASE_DATA_PATH}/images/{shard}", exist_ok=True)
+
+  logger.info(f"Processing images for shard {shard}...")
+
+  dataset = load_dataset(
+    "json", data_files=f"{BASE_DATA_PATH}/shards/{shard_filename}"
+  )["train"]
+
+  dataset.map(
+    _process,
+    batched=True,
+    batch_size=256,
+    num_proc=os.cpu_count(),
+  )
+
+  if os.path.exists(f"{BASE_DATA_PATH}/images/{shard}"):
+    logger.info(f"Zipping and uploading images for shard {shard}...")
+    hf_processor.zip_and_upload_dir(
+      dir_path=f"{BASE_DATA_PATH}/images/{shard}",
+      repo_id="Vividbot/llava-instruct-150k-vi",
+      path_in_repo=f"images/{shard}.zip",
+      repo_type="dataset",
+      overwrite=True,
+    )
+
+  end_time = time.time()
+  duration = round(end_time - start_time, 2)
+
+  logger.info(f"Processed shard {shard} in {duration} seconds")
+
+
+def prepare():
+  os.makedirs(f"{BASE_DATA_PATH}/images", exist_ok=True)
+
+
+def main():
+  prepare()
+  shard_files = os.listdir(f"{BASE_DATA_PATH}/shards")
+  shard_files = sorted(
+    shard_files,
+    key=lambda x: int(x.split(".")[0].split("_")[1]),
+  )
+
+  last_successful_shard = -1
+  # only process shards after the last successful shard
+  shard_files = shard_files[last_successful_shard + 1 :]
+
+  logger.info(f"Processing shards: {shard_files}")
+
+  for shard in tqdm(
+    shard_files,
+    desc="Processing shards",
+    unit="shard",
+    unit_scale=True,
+    unit_divisor=1024,
+  ):
+    process(shard)
+
+  send_completion_message()
+
+
+if __name__ == "__main__":
+  main()
---
+++ vividbot/data/task/llava_instruct_150k_vi/scripts/prepare_server.sh
@@ -0,0 +1,16 @@
+sudo apt update -y && \
+sudo apt install python3-pip unzip ffmpeg pipx -y && \
+pipx install poetry && pipx ensurepath && \
+source ~/.bashrc && \
+pip install "huggingface_hub[cli]" && \
+huggingface-cli login --token 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 && \
+git clone https://dminhvu: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@github.com/FPT-VVU/ViVidBot && \
+cd ViVidBot && \
+git checkout data/llava-instruct-150k-vi && \
+poetry install && \
+cd ~/ && mkdir llava-instruct-150k-vi && cd llava-instruct-150k-vi && \
+huggingface-cli download Vividbot/llava-instruct-150k-vi shards.zip --repo-type dataset --local-dir ./ && \
+unzip shards.zip && rm shards/shard_all.json \
+cd ~/ViVidBot
+
+poetry run python3 vividbot/data/task/llava_instruct_150k_vi/main.py
