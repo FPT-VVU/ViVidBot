dd6df3a981a25921344768d01a5546b2bb419e8c
Minh Vu <minhvu@codertrove.com>
2024-08-02 10:02:05 +0700

    fix merge model code

--- vividbot/experiment/llm-quantization/merge.py
+++ vividbot/experiment/llm-quantization/merge.py
@@ -1,11 +1,25 @@
-from peft import PeftModel
+import torch
+from peft import PeftConfig, PeftModel
 
 from vividbot.valley.model.valley_model import VividGPTForCausalLM
 
-base_model_name = "/home/ct-minhvu/vivid-4b"
-adapter_model_name = "/home/ct-minhvu/vivid-4b"
-model = VividGPTForCausalLM.from_pretrained(base_model_name)
-model = PeftModel.from_pretrained(model, adapter_model_name)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
 
-model = model.merge_and_unload()
-model.save_pretrained("merged_adapters")
+model_name = "/content/stage2/steps_15000"
+
+config = PeftConfig.from_pretrained(model_name)
+print(config)
+
+model = VividGPTForCausalLM.from_pretrained(
+  model_name, device_map="auto", torch_dtype=torch.float16
+).to(device)
+print(model)
+
+model = PeftModel.from_pretrained(
+  model, model_name, device_map="auto", torch_dtype=torch.float16
+).to(device)
+print(model)
+
+model = model.merge_and_unload().half()
+model.save_pretrained("/content/vivid-4b")
